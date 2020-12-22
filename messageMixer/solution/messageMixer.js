0eb60d79bd544de526713df1006aa0251ba94b83
Adrián Carmona <acarmona.dev@gmail.com>
2020-12-22 16:53:23 +0100

    finished first exercise

--- messageMixer/solution/messageMixer.js
+++ messageMixer/solution/messageMixer.js
@@ -1,57 +1,66 @@
-const MessageMixer = {
-    countCharacter(inputString, inputCharacter) {
-        let count = 0;
-        let string = inputString.toLowerCase();
-        let character = inputCharacter.toLowerCase();
-        for (let i = 0; i < string.length; i++) {
-            if (string[i] === character) {
-                count++;
-            }
+const countCharacter = (inputString, inputCharacter) => {
+    let count = 0;
+    let string = inputString.toLowerCase();
+    let character = inputCharacter.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === character) {
+            count++;
         }
-        return count;
-    },
-    capitalizeFirstCharacterOfWords(string) {
-        let arr = string.split(" ");
-        for (let i = 0; i < arr.length; i++) {
-            let word = arr[i];
-            arr[i] = word[0].toUpperCase() + word.substring(1);
-        }
-        return arr.join(" ");
-    },
-    reverseWord(word) {
-        return word.split("").reverse().join("");
-    },
-    reverseAllWords(sentence) {
-        let words = sentence.split(" ");
-        for (let i = 0; i < words.length; i++) {
-            words[i] = this.reverseWord(words[i]);
-        }
-        return words.join(" ");
-    },
-    replaceFirstOccurence(string, toBeReplaced, replaceWith) {
-        return string.replace(toBeReplaced, replaceWith);
-    },
-    replaceAllOccurrences(string, toBeReplaced, replaceWith) {
-        return string.split(toBeReplaced).join(replaceWith);
-    },
-    encode(string) {
-        let replacementObject = {
-            "a": "@",
-            "s": "$",
-            "i": "!",
-            "o": "0"
-        };
-        for (let key in replacementObject) {
-            string = this.replaceAllOccurrences(string, key, replacementObject[key]);
-        }
-        return string;
-    },
-    palindrome(str) {
-        return `${str} ${MessageMixer.reverseWord(str)}`;
-    },
-    pigLatin(sentence, character) {
-        return sentence.split(' ').join(character + ' ');
     }
+    return count;
+};
+const capitalizeFirstCharacterOfWords = (string) => {
+    let arr = string.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        let word = arr[i];
+        arr[i] = word[0].toUpperCase() + word.substring(1);
+    }
+    return arr.join(" ");
+};
+const reverseWord = (word) => {
+    return word.split("").reverse().join("");
+};
+const reverseAllWords = (sentence) => {
+    let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = reverseWord(words[i]);
+    }
+    return words.join(" ");
+};
+const replaceFirstOccurence = (string, toBeReplaced, replaceWith) => {
+    return string.replace(toBeReplaced, replaceWith);
+};
+const replaceAllOccurrences = (string, toBeReplaced, replaceWith) => {
+    return string.split(toBeReplaced).join(replaceWith);
+};
+const encode = (string) => {
+    let replacementObject = {
+        "a": "@",
+        "s": "$",
+        "i": "!",
+        "o": "0"
+    };
+    for (let key in replacementObject) {
+        string = replaceAllOccurrences(string, key, replacementObject[key]);
+    }
+    return string;
+};
+const palindrome = (str) => {
+    return `${str} ${reverseWord(str)}`;
+};
+const pigLatin = (sentence, character) => {
+    return sentence.split(' ').join(character + ' ');
 }
 
-export default MessageMixer;
+
+export {
+    countCharacter,
+    capitalizeFirstCharacterOfWords,
+    reverseWord,
+    reverseAllWords,
+    replaceFirstOccurence,
+    replaceAllOccurrences,
+    encode,
+    palindrome,
+    pigLatin
+};
